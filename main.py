9d071312d25df6bcfefb74a663bc7cad6bf3a841
root <root@DESKTOP-9INA4TA.localdomain>
2022-12-01 17:09:29 +0100

    :beers: naif algo

--- main.py
+++ main.py
@@ -96,115 +96,93 @@ def check_direction(board, y, x, sizeGame):
     return (-1, -1)
 
 def defend(board, sizeGame):
-    value = False
-    nb = 0
-    size = 0
     for y in range(sizeGame):
-        if value == True:
-            break
         for x in range(sizeGame):
-            max = 3
+            nb = 0
+            size = 0
+            max = 4
+            jump = False
+            tmp = 0
             if board[y][x] == '2':
-                # vertical
-                if (x + 1 < sizeGame and board[y][x + 1] == '2' or (x + 2 < sizeGame and board[y][x + 2] == "2")):
-                    while (nb < max):
-                        if (board[y][x + nb] == '-' and max <= 3):
-                            max += 1
-                            tmp = x + nb
-                        if (board[y][x + nb] == '2'):
-                            size += 1
-                        nb += 1
-                    if (max == 4 and size == 3 and board[y][tmp] == '-'):
-                        return (y, tmp)
-                    if (max == 3 and size == 3):
-                        if (x + nb <= sizeGame):
-                            if (board[y][x + nb] != '-'):
-                                if (x - 1 >= 0 and board[y][x - 1] == '-'):
-                                    return (y, x - 1)
-                                return (-1, -1)
-                            if (board[y][x + nb] == '-'):
-                                return (y, x + nb)
-                        if (x + nb > sizeGame):
-                            if (board[y][x - 1] == '-'):
-                                return (y, x - 1)
-                            return (-1, -1)
-                        # if (x + nb > sizeGame):
-                    
-                        # if (x + nb <= sizeGame):
-                        #     if (board[y][x - 1] == '-'):
-                        #         return (y, x - 1)
-                        #     return (-1, -1)
-                        # elif (x - 1 >= 0 and board[y][x + nb] != '-'):
-                        #     if (board[y][x - 1] == '-'):
-                        #         return (y, x - 1)
-                        #     return (-1, -1)
-                        # elif (board[y][x + nb] == '-'):
-                        #     return (y, x + nb)
                 # horizontal
-                # if (y + 1 < sizeGame and board[y + 1][x] == '2' or (y + 2 < sizeGame and board[y + 2][x] == "2")):
-                #     while (nb < max):
-                #         if (board[y + nb][x] == '-' and max <= 3):
-                #             max += 1
-                #             tmp = y + nb
-                #         if (board[y + nb][x] == '2'):
-                #             size += 1
-                #         nb += 1
-                #     if (max == 4 and board[tmp][x] == '-'):
-                #         return (tmp, x)
-                #     if (max == 3):
-                #         if (y + nb > sizeGame):
-                #             if (board[y - 1][x] == '-'):
-                #                 return (y - 1, x)
-                #             return (-1, -1)
-                #         elif (y - 1 >= 0 and board[y + nb][x] != '-'):
-                #             if (board[y - 1][x] == '-'):
-                #                 return (y - 1, x)
-                #             return (-1, -1)
-                #         elif (board[y + nb][x] == '-'):
-                #             return (y + nb, x)
-                # # diagonal left
-                # if (y + 1 <= sizeGame and x + 1 <= sizeGame and board[y + 1][x + 1] == '2' or (y + 2 < sizeGame and x + 2 < sizeGame and board[y + 2][x + 2] == "2")):
-                #     while (nb < max):
-                #         if (y + nb <= sizeGame and x + nb <= sizeGame and board[y + nb][x + nb] == '-' and max <= 3):
-                #             max += 1
-                #             tmp = y + nb
-                #             tmp2 = x + nb
-                #         if (y + nb <= sizeGame and x + nb <= sizeGame and board[y + nb][x + nb] == '2'):
-                #             size += 1
-                #         nb += 1
-                #     if (max == 4 and board[tmp][tmp2] == '-' and size == 3):
-                #         return (tmp, tmp2)
-                #     if (max == 3 and size == 3):
-                #         if (y + nb <= sizeGame and x + nb <= sizeGame):
-                #             if (board[y + nb][x + nb] == '-'):
-                #                 return (y + nb, x + nb)
-                #             elif (y - 1 >= 0 and x >= 0 and board[y - 1][x - 1] == '-'):
-                #                 return (y - 1, x - 1)
-                #         if ((y + nb > sizeGame or x + nb > sizeGame) and (y - 1 >= 0 and x - 1 >= 0) and board[y - 1][x - 1] == '-'):
-                #             return (y - 1, x - 1)
-                # # diagonal left
-                # if (y + 1 <= sizeGame and x - 1 <= sizeGame and board[y + 1][x - 1] == '2' or (y + 2 < sizeGame and x + 2 < sizeGame and board[y + 2][x + 2] == "2")):
-                #     while (nb < max):
-                #         if (y + nb <= sizeGame and x - nb <= sizeGame and board[y + nb][x - nb] == '-' and max <= 3):
-                #             max += 1
-                #             tmp = y + nb
-                #             tmp2 = x - nb
-                #         if (y + nb <= sizeGame and x - nb <= sizeGame and board[y + nb][x - nb] == '2'):
-                #             size += 1
-                #         nb += 1
-                #     if (max == 4 and board[tmp][tmp2] == '-' and size == 3):
-                #         return (tmp, tmp2)
-                #     if (max == 3 and size == 3):
-                #         if (y + nb <= sizeGame and x - nb <= sizeGame):
-                #             if (board[y + nb][x - nb] == '-'):
-                #                 return (y + nb, x - nb)
-                #             elif (y - 1 >= 0 and x >= 0 and board[y - 1][x - 1] == '-'):
-                #                 return (y - 1, x + 1)
-                #         if ((y + nb > sizeGame or x - nb > sizeGame) and (y - 1 >= 0 and x - 1 >= 0) and board[y - 1][x + 1] == '-'):
-                #             return (y - 1, x + 1)
-                # value = True
-                # y = sizeGame
-                # break
+                if (x + 1 <= sizeGame and board[y][x + 1] == '2' or (x + 2 <= sizeGame and board[y][x + 2] == "2")):
+                    while (x + nb <= sizeGame and board[y][x + nb] == '2'):
+                        if (x + nb + 2 <= sizeGame and x + nb + 1 <= sizeGame and board[y][x + nb + 1] == '-' and board[y][x + nb + 2] == '2'):
+                            jump = True
+                            tmp = x + nb + 1
+                            nb += 1
+                        size += 1
+                        nb += 1
+                    if (size >= 3):
+                        if (jump == True):
+                            return (y, tmp)
+                        if (x + nb <= sizeGame and board[y][x + nb] != '-' and size == 4):
+                            if (x - 1 >= 0 and board[y][x - 1] == '-'):
+                                return (y, x - 1)
+                        if (x + nb == sizeGame + 1 and board[y][x - 1] == '-'):
+                            return (y, x - 1)
+                        if (x + nb <= sizeGame and board[y][x + nb] == '-'):
+                            return (y, x + nb)
+                # vertical
+                if (y + 1 <= sizeGame and board[y + 1][x] == '2' or (y + 2 <= sizeGame and board[y + 2][x] == "2")):
+                    while (y + nb <= sizeGame and board[y + nb][x] == '2'):
+                        if (y + nb + 2 <= sizeGame and y + nb + 1 <= sizeGame and board[y + nb + 1][x] == '-' and board[y + nb + 2][x] == '2'):
+                            jump = True
+                            tmp = y + nb + 1
+                            nb += 1
+                        size += 1
+                        nb += 1
+                    if (size >= 3):
+                        if (jump == True):
+                            return (tmp, x)
+                        if (y + nb <= sizeGame and board[y + nb][x] != '-' and size == 4):
+                            if (y - 1 >= 0 and board[y - 1][x] == '-'):
+                                return (y - 1, x)
+                        if (y + nb == sizeGame + 1 and board[y - 1][x] == '-'):
+                            return (y - 1, x)
+                        if (y + nb <= sizeGame and board[y + nb][x] == '-'):
+                            return (y + nb, x)
+                # diagonal right
+                if (x + 1 <= sizeGame and y + 1 <= sizeGame and board[y + 1][x + 1] == '2' or (x + 2 <= sizeGame and y + 2 <= sizeGame and board[y + 2][x + 2] == "2")):
+                    while (x + nb <= sizeGame and y + nb <= sizeGame and board[y + nb][x + nb] == '2'):
+                        if (x + nb + 2 <= sizeGame and x + nb + 1 <= sizeGame and y + nb + 2 <= sizeGame and y + nb + 1 <= sizeGame and board[y + nb + 1][x + nb + 1] == '-' and board[y + nb + 2][x + nb + 2] == '2'):
+                            jump = True
+                            tmp = x + nb + 1
+                            tmp2 = y + nb + 1
+                            nb += 1
+                        size += 1
+                        nb += 1
+                    if (size >= 3):
+                        if (jump == True):
+                            return (tmp2, tmp)
+                        if (x + nb <= sizeGame and y + nb <= sizeGame and board[y + nb][x + nb] != '-' and size == 4):
+                            if (x - 1 >= 0 and y - 1 >= 0 and board[y - 1][x - 1] == '-'):
+                                return (y - 1, x - 1)
+                        if (x + nb == sizeGame + 1 and y + nb == sizeGame + 1 and board[y - 1][x - 1] == '-'):
+                            return (y - 1, x - 1)
+                        if (x + nb <= sizeGame and y + nb <= sizeGame and board[y + nb][x + nb] == '-'):
+                            return (y + nb, x + nb)
+                # diagonal left
+                if (x - 1 >= 0 and y + 1 <= sizeGame and board[y + 1][x - 1] == '2' or (x - 2 >= 0 and y + 2 <= sizeGame and board[y + 2][x - 2] == "2")):
+                    while (x - nb >= 0 and y + nb <= sizeGame and board[y + nb][x - nb] == '2'):
+                        if (x - nb - 2 >= 0 and x - nb - 1 >= 0 and y + nb + 2 <= sizeGame and y + nb + 1 <= sizeGame and board[y + nb + 1][x - nb - 1] == '-' and board[y + nb + 2][x - nb - 2] == '2'):
+                            jump = True
+                            tmp = x - nb - 1
+                            tmp2 = y + nb + 1
+                            nb += 1
+                        size += 1
+                        nb += 1
+                    if (size >= 3):
+                        if (jump == True):
+                            return (tmp2, tmp)
+                        if (x - nb >= 0 and y + nb <= sizeGame and board[y + nb][x - nb] != '-' and size == 4):
+                            if (x + 1 <= sizeGame and y - 1 >= 0 and board[y - 1][x + 1] == '-'):
+                                return (y - 1, x + 1)
+                        if (x - nb <= 0 and y + nb == sizeGame + 1 and board[y - 1][x + 1] == '-'):
+                            return (y - 1, x + 1)
+                        if (x - nb >= 0 and y + nb <= sizeGame and board[y + nb][x - nb] == '-'):
+                            return (y + nb, x - nb)
+                
     return (-1, -1)
 
 def play():
